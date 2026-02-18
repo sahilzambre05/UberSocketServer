9f388cd4ba3ec07a9d596b6aa1fceed3a886b0d1
sahilzambre05 <sahildinesh.zambre2023@vitstudent.ac.in>
2026-02-18 12:11:55 +0530

    Added ScheduleConfig and some TestController to test

--- src/main/java/com/example/UberSocketServer/controller/TestController.java
+++ src/main/java/com/example/UberSocketServer/controller/TestController.java
@@ -4,13 +4,31 @@ import com.example.UberSocketServer.dto.TestRequest;
 import com.example.UberSocketServer.dto.TestResponse;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class TestController {
+
+    private final SimpMessagingTemplate simpMessagingTemplate;
+
+
+    public TestController(SimpMessagingTemplate simpMessagingTemplate) {
+        this.simpMessagingTemplate = simpMessagingTemplate;
+    }
 
     @MessageMapping("/ping")
     @SendTo("/topic/ping")
     public TestResponse pingCheck(TestRequest message) {
         System.out.println("Received message from client " + message.getData());
         return TestResponse.builder().data("Received").build();
+    }
+
+
+    @Scheduled(fixedDelay = 2000)
+    public void sendPeriodicMessage() {
+        System.out.println("Executed periodic function");
+        simpMessagingTemplate.convertAndSend("/topic/scheduled", "Periodic Message sent " + System.currentTimeMillis());
     }
 }
